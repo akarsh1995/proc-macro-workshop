82f4f82970c983a8e039c8e17cc892095bd101cd
Akarsh Jain <akarsh.1995.02@gmail.com>
2023-08-23 09:59:38 +0530

    update(`builder/02-create-builder.rs`): CommandBuilder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,31 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{self, parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
 
-    TokenStream::new()
+    quote!(
+        pub struct CommandBuilder {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+
+        impl #name {
+            pub fn builder() -> CommandBuilder {
+                CommandBuilder {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    )
+    .into()
 }
+
