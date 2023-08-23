3c315c796816de07d8fad3f5b92afc2fd3437a98
Akarsh Jain <akarsh.1995.02@gmail.com>
2023-08-23 10:02:33 +0530

    update(`builder/02`): using type identfier

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,4 +1,5 @@
 use proc_macro::TokenStream;
+use proc_macro2::{Ident, Span};
 use quote::quote;
 use syn::{self, parse_macro_input, DeriveInput};
 
@@ -6,9 +7,10 @@ use syn::{self, parse_macro_input, DeriveInput};
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = input.ident;
+    let command_builder_type = Ident::new(&format!("{}Builder", name), Span::call_site());
 
     quote!(
-        pub struct CommandBuilder {
+        pub struct #command_builder_type {
             executable: Option<String>,
             args: Option<Vec<String>>,
             env: Option<Vec<String>>,
@@ -16,8 +18,8 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
 
         impl #name {
-            pub fn builder() -> CommandBuilder {
-                CommandBuilder {
+            pub fn builder() -> #command_builder_type {
+                #command_builder_type {
                     executable: None,
                     args: None,
                     env: None,
