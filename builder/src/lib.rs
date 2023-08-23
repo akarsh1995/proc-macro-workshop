459916563c1a0b773f50596a646982db5489ceba
Akarsh Jain <akarsh.1995.02@gmail.com>
2023-08-23 18:47:36 +0530

    update(`builder/06`): accept optional field

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,7 +1,10 @@
 use proc_macro::TokenStream;
 use proc_macro2::{Ident, Span};
 use quote::quote;
-use syn::{self, parse_macro_input, DeriveInput};
+use syn::{
+    self, parse_macro_input, AngleBracketedGenericArguments, DeriveInput, GenericArgument, Path,
+    PathArguments, Type, TypePath,
+};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -19,9 +22,27 @@ pub fn derive(input: TokenStream) -> TokenStream {
         unimplemented!()
     };
 
+    let ty_is_option = |ty: &Type| {
+        if let Type::Path(
+            TypePath {
+                path: Path { ref segments, .. },
+                ..
+            },
+            ..,
+        ) = ty
+        {
+            // the path could be std::option::Option also so taking just the last segment
+            return segments.len() > 0 && segments.last().unwrap().ident.to_string() == "Option";
+        }
+        false
+    };
+
     let builder_fields = fields.iter().map(|f| {
         let name = &f.ident;
         let ty = &f.ty;
+        if ty_is_option(ty) {
+            return quote! { #name: #ty };
+        }
         quote! { #name: std::option::Option<#ty> }
     });
 
@@ -33,16 +54,55 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let setters = fields.iter().map(|f| {
         let name = &f.ident;
         let ty = &f.ty;
-        quote! {
-            pub fn #name(&mut self, #name: #ty) -> &mut Self {
-                self.#name = Some(#name);
-                self
+
+        if ty_is_option(&ty) {
+            // extract the type inside Option<type>
+            if let Type::Path(
+                TypePath {
+                    path: Path { ref segments, .. },
+                    ..
+                },
+                ..,
+            ) = ty
+            {
+                // the path could be std::option::Option also so taking just the last segment
+                // and its inside angle bracketed args
+                let last_segment = segments.last().unwrap();
+                if let PathArguments::AngleBracketed(AngleBracketedGenericArguments {
+                    ref args,
+                    ..
+                }) = last_segment.arguments
+                {
+                    if let GenericArgument::Type(ref opt_inner_ty) = args[0] {
+                        return quote! {
+                            pub fn #name(&mut self, #name: #opt_inner_ty) -> &mut Self {
+                                self.#name = Some(#name);
+                                self
+                            }
+                        };
+                    }
+                }
+            }
+            unimplemented!()
+        } else {
+            quote! {
+                pub fn #name(&mut self, #name: #ty) -> &mut Self {
+                    self.#name = Some(#name);
+                    self
+                }
             }
         }
     });
 
     let set_fields = fields.iter().map(|f| {
         let name = &f.ident;
+
+        if ty_is_option(&f.ty) {
+            return quote! {
+                #name: self.#name.take()
+            };
+        }
+
         quote! {
             #name: self.#name.take().ok_or(
                 format!("{0} not set; use method {0} to set the {0}'s value.", stringify!(#name))
